7e05f89f5aabd89287fd52e10d40f32666bc5b1d
Matthew Wells <76452933+mattheww95@users.noreply.github.com>
2022-01-10 11:29:54 -0600

    Added Finalizesheet sh

---
+++ WW_VCFParser/scripts/FinalizeSheet.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+SubmittersDir=./SubmittersDir
+
+for loc in $SubmittersDir/*/*/
+do
+  for i in $(cat $loc/*_loc_parser.txt)
+  do
+    basename $i | cut -d. -f1 | xargs -I cody echo -e cody"\t"$i  
+  done | sort -u -k1 >> $loc/$(basename $loc)_vcfparser_sorted_ready.txt 
+done
