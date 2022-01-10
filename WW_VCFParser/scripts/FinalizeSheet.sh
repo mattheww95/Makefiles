52925e1e4c61acbf1296746568e5f84a9c03543a
Matthew Wells <76452933+mattheww95@users.noreply.github.com>
2022-01-10 11:34:00 -0600

    Fixed sorting behaviour

--- WW_VCFParser/scripts/FinalizeSheet.sh
+++ WW_VCFParser/scripts/FinalizeSheet.sh
@@ -7,5 +7,5 @@ do
   for i in $(cat $loc/*_loc_parser.txt)
   do
     basename $i | cut -d. -f1 | xargs -I cody echo -e cody"\t"$i  
-  done | sort -u -k1 >> $loc/$(basename $loc)_vcfparser_sorted_ready.txt 
+  done | sort -u -k1,1 >> $loc/$(basename $loc)_vcfparser_sorted_ready.txt 
 done
